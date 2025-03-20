15f28ac5f90b2e308d90efb14add945bca5acbd6
genesisTerraria <abca41833@gmail.com>
2025-03-20 01:19:09 -0500

    Update ShardstonePickaxe.cs

--- CTG2/Content/Items/ShardstonePickaxe.cs
+++ CTG2/Content/Items/ShardstonePickaxe.cs
@@ -1,1 +1,33 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace CTG2.Content.Items
+{
+	public class idk : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.damage = 5;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 36;
+			Item.height = 36;
+			Item.useTime = 18;
+			Item.useAnimation = 18;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 5;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.LightRed;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 6969);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
