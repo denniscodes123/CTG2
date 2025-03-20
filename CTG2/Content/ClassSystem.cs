3b332b90d0a98dbc1e5934fea036f07b1af3f608
genesisTerraria <abca41833@gmail.com>
2025-03-19 22:47:05 -0500

    Update ClassSystem.cs

--- CTG2/Content/ClassSystem.cs
+++ CTG2/Content/ClassSystem.cs
@@ -113,6 +113,48 @@ public class ClassSystem : ModPlayer
                     Player.QuickSpawnItem(null, ItemID.WizardHat);
                     Player.QuickSpawnItem(null, ItemID.ManaCrystal, 3);
                     break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+                case 11:
+                    break;
+
+                case 12:
+                    break;
+
+                case 13:
+                    break;
+
+                case 14:
+                    break;
+
+                case 15:
+                    break;
+
+                case 16:
+                    break;
+
+                case 17:
+                    break;
             }
 
             hasReceivedItems = true;
