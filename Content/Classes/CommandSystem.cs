f0f7fe679941fcfb66eeca26499857bf7df2b29d
genesisTerraria <abca41833@gmail.com>
2025-06-15 22:10:14 -0500

    Update CommandSystem.cs

--- Content/Classes/CommandSystem.cs
+++ Content/Classes/CommandSystem.cs
@@ -34,92 +34,92 @@ public class ClassCommand : ModCommand
         {
             case 1:
                 modPlayer.playerClass = 1;
-                caller.Reply("You are now an Archer!", Color.Green);
+                caller.Reply("You selected Archer.", Color.Green);
                 break;
 
             case 2:
                 modPlayer.playerClass = 2;
-                caller.Reply("You are now ninja!", Color.Green);
+                caller.Reply("You selected Ninja.", Color.Green);
                 break;
 
             case 3:
                 modPlayer.playerClass = 3;
-                caller.Reply("You are now the beast!", Color.Green);
+                caller.Reply("You selected Beast.", Color.Green);
                 break;
 
             case 4:
                 modPlayer.playerClass = 4;
-                caller.Reply("You are now the gladiator!", Color.Green);
+                caller.Reply("You selected Gladiator.", Color.Green);
                 break;
 
             case 5:
                 modPlayer.playerClass = 5;
-                caller.Reply("You are now paladin!", Color.Green);
+                caller.Reply("You selected Paladin.", Color.Green);
                 break;
 
             case 6:
                 modPlayer.playerClass = 6;
-                caller.Reply("You are now jman!", Color.Green);
+                caller.Reply("You selected Jungle Man.", Color.Green);
                 break;
 
             case 7:
                 modPlayer.playerClass = 7;
-                caller.Reply("You are now bmage!", Color.Green);
+                caller.Reply("You selected Black Mage.", Color.Green);
                 break;
 
             case 8:
                 modPlayer.playerClass = 8;
-                caller.Reply("You are now a psychic!", Color.Green);
+                caller.Reply("You selected Psychic.", Color.Green);
                 break;
 
             case 9:
                 modPlayer.playerClass = 9;
-                caller.Reply("You are now wmage!", Color.Green);
+                caller.Reply("You selected White Mage.", Color.Green);
                 break;
 
             case 10:
                 modPlayer.playerClass = 10;
-                caller.Reply("You are now miner!", Color.Green);
+                caller.Reply("You selected Miner.", Color.Green);
                 break;
 
             case 11:
                 modPlayer.playerClass = 11;
-                caller.Reply("You are now fish!", Color.Green);
+                caller.Reply("You selected Fish.", Color.Green);
                 break;
 
             case 12:
                 modPlayer.playerClass = 12;
-                caller.Reply("You are now clown!", Color.Green);
+                caller.Reply("You selected Clown.", Color.Green);
                 break;
 
             case 13:
                 modPlayer.playerClass = 13;
-                caller.Reply("You are now fbunny!", Color.Green);
+                caller.Reply("You selected Flame Bunny.", Color.Green);
                 break;
 
             case 14:
                 modPlayer.playerClass = 14;
-                caller.Reply("You are now tiki priest!", Color.Green);
+                caller.Reply("You selected Tiki Priest.", Color.Green);
                 break;
 
             case 15:
                 modPlayer.playerClass = 15;
-                caller.Reply("You are now tree!", Color.Green);
+                caller.Reply("You selected Tree.", Color.Green);
                 break;
 
             case 16:
                 modPlayer.playerClass = 16;
-                caller.Reply("You are now mutant!", Color.Green);
+                caller.Reply("You selected Mutant.", Color.Green);
                 break;
 
             case 17:
                 modPlayer.playerClass = 17;
-                caller.Reply("You are now leech!", Color.Green);
+                caller.Reply("You selected Leech.", Color.Green);
                 break;
 
 
             default:
-                caller.Reply("Invalid class. Available: 1 (Archer), 2 (Warrior), 3 (Mage)", Color.Red);
+                caller.Reply("Invalid class.", Color.Red);
                 break;
         }
     }
