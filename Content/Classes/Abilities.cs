433b87ec0627402ae9cf5d0f3dc9a595045c015b
genesisTerraria <abca41833@gmail.com>
2025-06-16 13:55:02 -0500

    Update Abilities.cs

--- Content/Classes/Abilities.cs
+++ Content/Classes/Abilities.cs
@@ -15,127 +15,51 @@ namespace CTG2.Content
         private int class6FlameDuration = 0;
 
 
-        public override void PostItemCheck()
+        private void SetCooldown(int seconds)
         {
-            if (Player.HeldItem.type == ItemID.WhoopieCushion &&
-                Player.controlUseItem &&
-                Player.itemTime == 0 &&
-                !Player.HasBuff(BuffID.ChaosState)) // Only activate if not on cooldown
+            Player.AddBuff(BuffID.ChaosState, seconds * 60);
+        }
+
+
+        private void ArcherOnUse() //not finished
+        {
+
+        }
+
+
+        private void NinjaOnUse()
+        {
+            Player.AddBuff(BuffID.Invisibility, 60 * 60);
+        }
+
+
+        private void BeastOnUse()
+        {
+            Player.AddBuff(BuffID.MagicPower, 600);
+
+            if (Main.myPlayer == Player.whoAmI && Main.netMode != NetmodeID.MultiplayerClient) // server-side or singleplayer
             {
-                int selectedClass = Player.GetModPlayer<ClassSystem>().playerClass;
-
-                switch (selectedClass)
-                {
-                    case 1: // not finished 
-                        Player.AddBuff(BuffID.ChaosState, 36 * 60);
-                        Player.AddBuff(BuffID.Swiftness, 600);
-
-                        break;
-
-                    case 2:
-                        Player.AddBuff(BuffID.Invisibility, 3600);
-                        Player.AddBuff(BuffID.ChaosState, 600);
-                        break;
-
-                    case 3:
-                        Player.AddBuff(BuffID.ChaosState, 35 * 60);
-                        Player.AddBuff(BuffID.MagicPower, 600);
-
-                        if (Main.myPlayer == Player.whoAmI && Main.netMode != NetmodeID.MultiplayerClient) // server-side or singleplayer
-                        {
-                            int npcIndex = NPC.NewNPC(Player.GetSource_Misc("Class3Ability"), (int)Player.Center.X, (int)Player.Center.Y, ModContent.NPCType<StationaryBeast>());
-                            if (Main.netMode == NetmodeID.Server)
-                                NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npcIndex);
-                        }
-                        break;
-
-
-                    case 4: //finished
-                        Player.AddBuff(206, 300);
-                        Player.AddBuff(195, 300);
-                        Player.AddBuff(75, 300);
-                        Player.AddBuff(320, 300);
-                        Player.AddBuff(BuffID.ChaosState, 35 * 60);
-
-                        class4BuffTimer = 300;
-                        class4PendingBuffs = true;
-
-                        //5 second interval here (postupdateworld runs the interval)
-                        break;
-
-                    case 5: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 10 * 60);
-
-                        break;
-
-                    case 6: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 31 * 60);
-                        Player.AddBuff(149, 42);
-                        Player.AddBuff(114, 42);
-                        
-                        class6FlameDuration = 1;
-                        break;
-
-                    case 7:
-                        Player.AddBuff(BuffID.ChaosState, 42 * 60);
-                        Player.AddBuff(176, 15);
-                        Player.AddBuff(26, 420);
-                        Player.AddBuff(137, 420);
-                        Player.AddBuff(320, 420);
-                        break;
-
-                    case 8: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 40 * 60);
-                        break;
-
-                    case 9: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 30 * 60);
-                        break;
-
-                    case 10: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 15 * 60);
-                        break;
-
-                    case 11:
-                        Player.AddBuff(BuffID.ChaosState, 35 * 60);
-                        Player.AddBuff(1, 180);
-                        Player.AddBuff(104, 180);
-                        Player.AddBuff(109, 180);
-                        break;
-
-                    case 12: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 11 * 60);
-                        break;
-
-                    case 13: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 41 * 60);
-                        break;
-
-                    case 14: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 20 * 60);
-                        break;
-
-                    case 15: //not finished 
-                        Player.AddBuff(BuffID.ChaosState, 27 * 60);
-                        break;
-
-                    case 16: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 1 * 60);
-                        break;
-
-                    case 17: //not finished
-                        Player.AddBuff(BuffID.ChaosState, 40 * 60);
-                        break;
-
-                }
+                int npcIndex = NPC.NewNPC(Player.GetSource_Misc("Class3Ability"), (int)Player.Center.X, (int)Player.Center.Y, ModContent.NPCType<StationaryBeast>());
+                if (Main.netMode == NetmodeID.Server) NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npcIndex);
             }
         }
 
-        //All timer logic below
-        public override void PostUpdate()
-        {
 
-            if (class4PendingBuffs) //runs 5 second interval for glad ability 
+        private void GladiatorOnUse()
+        {
+            Player.AddBuff(206, 300);
+            Player.AddBuff(195, 300);
+            Player.AddBuff(75, 300);
+            Player.AddBuff(320, 300);
+
+            class4BuffTimer = 300;
+            class4PendingBuffs = true;
+        }
+
+
+        private void GladiatorPostStatus()
+        {
+            if (class4PendingBuffs) // runs 5 second interval
             {
                 class4BuffTimer--;
 
@@ -151,13 +75,28 @@ namespace CTG2.Content
                     class4PendingBuffs = false;
                 }
             }
+        }
 
 
+        private void PaladinOnUse() //not finished
+        {
+
+        }
 
 
-            if (class6FlameDuration > 0) //jman logic
+        private void JungleManOnUse()
+        {
+            Player.AddBuff(149, 42);
+            Player.AddBuff(114, 42);
+                        
+            class6FlameDuration = 1;
+        }
+
+
+        private void JungleManPostStatus()
+        {
+            if (class6FlameDuration > 0)
             {
-
                 class6FlameDuration--;
 
                 if (Main.myPlayer == Player.whoAmI && Main.netMode != NetmodeID.MultiplayerClient)
@@ -182,24 +121,215 @@ namespace CTG2.Content
                         if (Main.myPlayer == Player.whoAmI && Main.netMode != NetmodeID.MultiplayerClient)
                         {
                             Projectile.NewProjectile(
-                            Player.GetSource_Misc("Class6GroundFlames"),
-                            spawnPoss,
-                            velocity,
-                            480, // cursed flame
-                            26,
-                            1f,
-                            Player.whoAmI
-                             );
+                                Player.GetSource_Misc("Class6GroundFlames"),
+                                spawnPoss,
+                                velocity,
+                                480, // cursed flame
+                                26,
+                                1f,
+                                Player.whoAmI
+                            );
                         }
                     }
+                }
+            }
+        }
 
+
+        private void BlackMageOnUse()
+        {
+            Player.AddBuff(176, 15);
+            Player.AddBuff(26, 420);
+            Player.AddBuff(137, 420);
+            Player.AddBuff(320, 420);
+        }
+
+
+        private void PsychicOnUse() //not finished
+        {
+
+        }
+
+
+        private void WhiteMageOnUse() //not finished
+        {
+
+        }
+
+
+        private void MinerOnUse() //not finished
+        {
+
+        }
+
+
+        private void FishOnUse()
+        {
+            Player.AddBuff(1, 180);
+            Player.AddBuff(104, 180);
+            Player.AddBuff(109, 180);
+        }
+
+
+        private void ClownOnUse() //not finished
+        {
+
+        }
+
+
+        private void FlameBunnyOnUse() //not finished
+        {
+
+        }
+
+
+        private void TikiPriestOnUse() //not finished
+        {
+
+        }
+
+
+        private void TreeOnUse() //not finished
+        {
+
+        }
+
+
+        private void MutantOnUse() //not finished
+        {
+
+        }
+
+
+        private void LeechOnUse() //not finished
+        {
+
+        }
+
+
+        public override void PostItemCheck() // Upon activation
+        {
+            if (Player.HeldItem.type == ItemID.WhoopieCushion &&
+                Player.controlUseItem &&
+                Player.itemTime == 0 &&
+                !Player.HasBuff(BuffID.ChaosState)) // Only activate if not on cooldown
+            {
+                
+                int selectedClass = Player.GetModPlayer<ClassSystem>().playerClass;
+
+                switch (selectedClass)
+                {
+                    case 1:
+                        SetCooldown(36);
+                        ArcherOnUse();
+
+                        break;
+
+                    case 2:
+                        SetCooldown(10);
+                        NinjaOnUse();
+                        
+                        break;
+
+                    case 3:
+                        SetCooldown(35);
+                        BeastOnUse();
+
+                        break;
+
+                    case 4: //finished
+                        SetCooldown(35);
+                        GladiatorOnUse();
+
+                        break;
+
+                    case 5: //not finished
+                        SetCooldown(10);
+                        PaladinOnUse();
+
+                        break;
+
+                    case 6:
+                        SetCooldown(31);
+                        JungleManOnUse();
+
+                        break;
+
+                    case 7:
+                        SetCooldown(42);
+                        BlackMageOnUse();
+
+                        break;
+
+                    case 8: //not finished
+                        SetCooldown(40);
+                        PsychicOnUse();
+
+                        break;
+
+                    case 9: //not finished
+                        SetCooldown(30);
+                        WhiteMageOnUse();
+
+                        break;
+
+                    case 10: //not finished
+                        SetCooldown(15);
+                        MinerOnUse();
+
+                        break;
+
+                    case 11:
+                        SetCooldown(35);
+                        FishOnUse();
+
+                        break;
+
+                    case 12: //not finished
+                        SetCooldown(11);
+                        ClownOnUse();
+
+                        break;
+
+                    case 13: //not finished
+                        SetCooldown(41);
+                        FlameBunnyOnUse();
+
+                        break;
+
+                    case 14: //not finished
+                        SetCooldown(20);
+                        TikiPriestOnUse();
+
+                        break;
+
+                    case 15: //not finished 
+                        SetCooldown(27);
+                        TreeOnUse();
+
+                        break;
+
+                    case 16: //not finished
+                        SetCooldown(1);
+                        MutantOnUse();
+
+                        break;
+
+                    case 17: //not finished
+                        SetCooldown(40);
+                        LeechOnUse();
+
+                        break;
 
                 }
             }
         }
+
+        //All timer logic below
+        public override void PostUpdate()
+        {
+            GladiatorPostStatus();
+            JungleManPostStatus();
         }
-
-
     }
-    
-
+}
