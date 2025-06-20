008792d29953b8e74be310e71f5887f650ccde01
genesisTerraria <abca41833@gmail.com>
2025-06-19 22:00:00 -0500

    Update UpdatedWeps.cs

--- Content/ModifiedWeps/UpdatedWeps.cs
+++ Content/ModifiedWeps/UpdatedWeps.cs
@@ -6,38 +6,49 @@ namespace CTG2.Content.Items.ModifiedWeps
 {
     public class OverloadedWeps : GlobalItem
     {
+
+        private uint bananarangDelay = 55;
+        private uint bananarangLastUsedCounter = 0;
+
+        public override bool InstancePerEntity => true;
+
+
         public override void SetDefaults(Item item)
         {
             switch (item.type)
             {
-                case ItemID.TendonBow:
-                    item.useTime = 40;
-                    item.useAnimation = 30;
-                    item.damage = 40;
-                    item.knockBack = 3.5f;
-                    item.autoReuse = true;
-                    item.mana = 5;
-                    item.DamageType = DamageClass.Magic;
-                    break;
-
-                case ItemID.EnchantedSword:
-                    item.damage = 100;
-                    item.knockBack = 8f;
-                    item.autoReuse = true;
-                    break;
-
-                case ItemID.Starfury:
-                    item.useTime = 12;
-                    item.useAnimation = 12;
-                    item.damage = 60;
-                    item.shootSpeed = 10f;
-                    break;
-
                 case ItemID.WhoopieCushion:
                     item.useTime = 1;
                     item.useAnimation = 1;
                     break;
+
+                case ItemID.Bananarang:
+                    item.useTime = 20;
+                    item.useAnimation = 20;
+                    item.shootSpeed = 11.4f;
+                    item.knockBack = 6f;
+                    item.autoReuse = false;
+                    item.damage = 35;
+                    break;
             }
         }
+
+
+        public override bool CanUseItem(Item item, Player player)
+        {
+            if (item.type == ItemID.Bananarang)
+            {
+                if (Main.GameUpdateCount - bananarangLastUsedCounter >= bananarangDelay)
+                {
+                    bananarangLastUsedCounter = Main.GameUpdateCount;
+        
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return true;
+	 	}
     }
 }
