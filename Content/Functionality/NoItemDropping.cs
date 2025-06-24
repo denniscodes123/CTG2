ab5634b4d4dcfbdf6dc1ed02d9d64dd9204af2d3
denniscodes123 <dadennis2005@gmail.com>
2025-06-24 01:35:52 -0400

    Made it so players can't drop their items and give to others when match active

---
+++ Content/Functionality/NoItemDropping.cs
@@ -0,0 +1,34 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using CTG2.Content.ClientSide;
+namespace CTG2.Content.Functionality;
+
+public class DropBlockSystem : ModSystem
+{
+
+
+public override void PreUpdateEntities()
+    {
+        for (int i = 0; i < Main.maxItems; i++)
+        {
+            Item item = Main.item[i];
+
+if (item.velocity.Y == -2f && item.active && (GameInfo.matchStage==1 || GameInfo.matchStage==2)) 
+    //Dropped items y gets set to -2 (items from /item and breaking blocks don't get turned to air)
+    item.TurnToAir();
+    NetMessage.SendData(MessageID.SyncItem, -1, -1, null, i);
+    if (Main.myPlayer == i)
+        Main.NewText("Dropping items is disabled.", Color.Red);
+}
+        }
+    }
+
+
+
+
+
+
+
