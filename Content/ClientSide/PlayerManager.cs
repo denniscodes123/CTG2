b559230a6f988505da4bf76bdac66444a1bbcfad
denniscodes123 <dadennis2005@gmail.com>
2025-06-25 01:40:16 -0400

    Made players ghost while awaiting respawn (doesn't use player.respawntimer anymore)
    Haven't tried yet but might be able to grab gem when standing inside it and respawn timer ends

--- Content/ClientSide/PlayerManager.cs
+++ Content/ClientSide/PlayerManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text.Json;
 using ClassesNamespace;
@@ -12,45 +12,58 @@ public class PlayerManager : ModPlayer
 {   
     public static bool ShowClassUI = false;
     public static bool ShowGameUI = false;
-    
+    public int customRespawnTimer = -1;
+    public bool awaitingRespawn = false;
+
+
     // Set Custom Respawn Times
     public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
     {
         var modPlayer = Player.GetModPlayer<ClassSystem>();
 
+        if (GameInfo.matchStage == 2)
+        {
+            awaitingRespawn = true;
+            Player.ghost = true;
+            Player.dead = true;
+        }
+
         // How much time has passed since match started
-        int timeElapsed = GameInfo.matchTime/60 - 30;
+        int timeElapsed = GameInfo.matchTime / 60 - 30;
         int extraSeconds = Math.Max(0, timeElapsed / 120); // +1s for every 2 minutes
 
+        Player.respawnTimer = 0;
         switch (modPlayer.playerClass)
         {
             case GameClass.Archer: // Archer
-                Player.respawnTimer = (3 + extraSeconds) * 60;
+                customRespawnTimer = (3 + extraSeconds) * 60;
                 break;
 
-            case GameClass.Ninja: 
-                Player.respawnTimer = (1 + extraSeconds) * 60;
+            case GameClass.Ninja:
+                customRespawnTimer = (1 + extraSeconds) * 60;
                 break;
 
-            case GameClass.Beast: 
-                Player.respawnTimer = (2 + extraSeconds) * 60;
+            case GameClass.Beast:
+                customRespawnTimer = (2 + extraSeconds) * 60;
                 break;
 
             default:
-                Player.respawnTimer = (3 + extraSeconds) * 60; 
+                customRespawnTimer = (3 + extraSeconds) * 60;
                 break;
         }
+
     }
-    
+
     // Set Custom Spawn Points
     public override void OnRespawn()
-    {   
+    {
+
         int blueBaseX = 12346 / 16;
         int blueBaseY = 10940 / 16;
         int redBaseX = 20385 / 16;
         int redBaseY = 10940 / 16;
         switch (GameInfo.matchStage)
-        { 
+        {
             case 0:
                 break;
             case 1:
@@ -78,6 +91,7 @@ public class PlayerManager : ModPlayer
                 }
                 break;
         }
+        
     }
 
     public override void PreUpdate()
@@ -85,9 +99,9 @@ public class PlayerManager : ModPlayer
         if (GameInfo.matchStage == 1)
         {
             if (GameInfo.matchTime == 1795)
-            {   
+            {
                 var player = Main.LocalPlayer.GetModPlayer<ClassSystem>();
-                
+
                 if (player.playerClass == GameClass.None)
                 {
                     Main.NewText($"You did not select a class! Assigning a random class.");
@@ -97,6 +111,26 @@ public class PlayerManager : ModPlayer
                 }
             }
         }
+            if (awaitingRespawn) //was lowkey angry while coding this will clean up later
+    {
+        customRespawnTimer--;
+
+        if (customRespawnTimer <= 0)
+        {
+            awaitingRespawn = false;
+
+            Player.ghost = false;
+            Player.dead = true;
+
+            Player.statLife = Player.statLifeMax2;
+            Player.HealEffect(Player.statLifeMax2);
+
+            
+            
+        }
+    }
+
+
 
     }
 
