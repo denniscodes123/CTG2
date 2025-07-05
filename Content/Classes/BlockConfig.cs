0fc151b31a29241b9a56a072f454dad03109decf
denniscodes123 <dadennis2005@gmail.com>
2025-07-05 01:29:33 -0400

    Added block config (50 dirt per 30 seconds rn)

---
+++ Content/Classes/BlockConfig.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CTG2.Content.ClientSide;
+namespace CTG2.Content.Classes;
+
+public class BlockRewardSystem : ModSystem
+{
+    private double blockTimer = 0;
+    public static bool canBeDropped;
+    public override void PostUpdatePlayers()
+    {
+        blockTimer += 1.0 / 60.0;
+        ;
+
+        if (blockTimer < 30.0)
+            return;
+
+        blockTimer = 0;
+
+        if (GameInfo.matchStage != 2)
+            return;
+
+        canBeDropped = true;
+        for (int i = 0; i < Main.maxPlayers; i++)
+        {
+            Player player = Main.player[i];
+            if (!player.active || player.dead)
+                continue;
+
+            if (player.team == 0)
+                continue;
+
+            player.QuickSpawnItem(null, ItemID.DirtBlock, 50);
+        }
+        canBeDropped = false;
+    }
+}
