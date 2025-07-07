130ad2972961c611f2594da40ad9944313710d04
denniscodes123 <dadennis2005@gmail.com>
2025-07-07 03:35:01 -0400

    Update Zen.cs

--- Content/Items/Zen.cs
+++ Content/Items/Zen.cs
@@ -6,11 +6,17 @@ using Microsoft.Xna.Framework;
 namespace CTG2.Content.Items {
 	
 	public class Zen : ModItem { // Magic Dagger
-	    
-		public override void SetDefaults() {
-	  		Item.CloneDefaults(ItemID.MagicDagger);
+
+		public override void SetDefaults()
+		{
+			Item.CloneDefaults(ItemID.MagicDagger);
 
 			Item.shoot = ModContent.ProjectileType<ZenProjectile>();
+			Item.useAnimation = 10;
+			Item.useTime = 10;
+			Item.shootSpeed = 10f;
+			Item.crit = 0;
+			Item.damage = 36;
 		}
 
 	}
