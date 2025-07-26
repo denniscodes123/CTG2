e8e30c87c4ab34026f91df26ce0566d13248c1d3
genesisTerraria <abca41833@gmail.com>
2025-07-26 18:58:22 -0500

    Update TikiTotem.cs

--- Content/Classes/TikiTotem.cs
+++ Content/Classes/TikiTotem.cs
@@ -5,6 +5,8 @@ using Terraria.ID;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent;
+using Microsoft.Xna.Framework.Audio;
+using Terraria.Audio;
 
 namespace CTG2.Content.Classes
 {
@@ -21,9 +23,12 @@ namespace CTG2.Content.Classes
     {
 
         private float healFrameGap = 30;
+        private int hitCounter = 0;
         private float frameCount = 0;
         private int totemTeam = 0;
         private int maxHP = 200;
+
+        SoundStyle totemCrumble = new SoundStyle("CTG2/Content/Classes/TotemCrumble");
 
         public override void SetStaticDefaults()
         {
@@ -104,6 +109,69 @@ namespace CTG2.Content.Classes
             // Main.NewText(NPC.GetGlobalNPC<AllNpcs>().team);
         }
 
+
+        public override void OnHitByItem(Player player, Item item, NPC.HitInfo hit, int damageDone)
+        {
+            int tikiTeam = (int)NPC.ai[0];
+
+            if (NPC.life <= 0)
+            {
+                if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 90);
+                else if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 88);
+
+                SoundEngine.PlaySound(totemCrumble.WithVolumeScale(Main.soundVolume * 1f), NPC.Center);
+            }
+            else
+            {
+                if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 60);
+                else if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 59);
+            }
+
+            SoundEngine.PlaySound(SoundID.NPCHit15, NPC.Center);
+
+            hitCounter = 1;
+        }
+
+
+        public override void OnHitByProjectile (Projectile projectile, NPC.HitInfo hit, int damageDone)
+        {
+            int tikiTeam = (int)NPC.ai[0];
+
+            if (NPC.life <= 0)
+            {
+                if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 90);
+                else if (tikiTeam == 1)
+                    for (int i = 0; i < 5; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 88);
+
+                SoundEngine.PlaySound(totemCrumble.WithVolumeScale(Main.soundVolume * 6f), NPC.Center);
+            }
+            else
+            {
+                if (tikiTeam == 1)
+                    for (int i = 0; i < 10; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 60);
+                else if (tikiTeam == 1)
+                    for (int i = 0; i < 10; i++)
+                        Dust.NewDust(NPC.position, NPC.width, NPC.height, 59);
+            }
+
+            SoundEngine.PlaySound(SoundID.NPCHit16, NPC.Center);
+
+            hitCounter = 1;
+        }
+
+
         public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
             Texture2D texture = TextureAssets.Npc[NPC.type].Value;
@@ -112,10 +180,29 @@ namespace CTG2.Content.Classes
             Color teamColar = Color.Gray;
 
             int tikiTeam = (int)NPC.ai[0];
-            if (tikiTeam == 1){ teamColar = new Color(255, 0, 0, 180); }
-            if (tikiTeam == 3){ teamColar = new Color(0, 0, 255, 180); }
+            if (hitCounter > 0 && hitCounter <= 50)
+            {
+                if (tikiTeam == 1)
+                    teamColar = new Color(255, hitCounter * 5, hitCounter * 5, 155 + hitCounter * 2);
+                else if (tikiTeam == 3)
+                    teamColar = new Color(hitCounter * 5, hitCounter * 5, 255, 155 + hitCounter * 2);
+            }
+            else if (hitCounter > 50)
+            {
+                if (tikiTeam == 1)
+                    teamColar = new Color(255, 250 - (hitCounter - 50) * 5, 250 - (hitCounter - 50) * 5, 255 - (hitCounter - 50) * 2);
+                else if (tikiTeam == 3)
+                    teamColar = new Color(250 - (hitCounter - 50) * 5, 250 - (hitCounter - 50) * 5, 255, 255 - (hitCounter - 50) * 2);
+            }
+            else
+            {
+                if (tikiTeam == 1)
+                    teamColar = new Color(255, 0, 0, 155);
+                else if (tikiTeam == 3)
+                    teamColar = new Color(0, 0, 255, 155);
+            }
 
-                Vector2 drawPosition = NPC.Center - screenPos + new Vector2(0, 2.8f);
+            Vector2 drawPosition = NPC.Center - screenPos + new Vector2(0, 2.8f);
 
             spriteBatch.Draw(
                 texture,
@@ -128,6 +215,9 @@ namespace CTG2.Content.Classes
                 SpriteEffects.None,
                 0f
             );
+
+            if (hitCounter > 0 && hitCounter < 100) hitCounter++;
+            else if (hitCounter >= 50) hitCounter = 0;
         }
     }
 }
