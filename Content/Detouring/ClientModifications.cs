b57060a37d06174e12095e6f4093ce5b300011e6
pbjrulz@gmail.com <pbjrulz@gmail.com>
2025-07-28 13:23:53 -0700

    broken IL chat code

--- Content/Detouring/ClientModifications.cs
+++ Content/Detouring/ClientModifications.cs
@@ -16,6 +16,7 @@ using Terraria.Enums;
 using ClassesNamespace;
 using Mono.Cecil.Cil;
 using System.Reflection;
+using Terraria.GameContent.UI.Chat;
 
 namespace CTG2.Detouring;
 
@@ -24,8 +25,9 @@ public class ClientModifications : ModSystem
     public override void Load()
     {
         IL_Player.TeamChangeAllowed += TeamChangeFlag;
+        IL_ChatHelper.DisplayMessage += ChatHelper_DisplayMessage_Hook;
         var drawPVPIconsMethodInfo = typeof(Main).GetMethod("DrawPVPIcons", BindingFlags.NonPublic | BindingFlags.Static);
-                if (drawPVPIconsMethodInfo != null)
+        if (drawPVPIconsMethodInfo != null)
         {
             IL_Main.DrawPVPIcons += Main_DrawPVPIcons_Hook;
         }
@@ -34,13 +36,26 @@ public class ClientModifications : ModSystem
             // Log an error if we can't find the method, which might happen if the game updates.
             Mod.Logger.Error("Could not find Main.DrawPVPIcons method. PvP admin check will not work.");
         }
-        
+        var drawBubblesMethodInfo = typeof(Main).GetMethod("DrawPlayerChatBubbles", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (drawBubblesMethodInfo != null)
+        {
+            // Apply our hook to the method.
+            IL_Main.DrawPlayerChatBubbles += DisableChatBubbles_Hook;
+        }
+        else
+        {
+            // This is a safety check in case the method name changes in a future Terraria update.
+            Mod.Logger.Error("Could not find Main.DrawPlayerChatBubbles method. Chat bubble disabling will not work.");
+        }
+
     }
     public override void Unload()
     {
         IL_Player.TeamChangeAllowed -= TeamChangeFlag;
+        IL_ChatHelper.DisplayMessage -= ChatHelper_DisplayMessage_Hook;
         var drawPVPIconsMethodInfo = typeof(Main).GetMethod("DrawPVPIcons", BindingFlags.NonPublic | BindingFlags.Static);
-                if (drawPVPIconsMethodInfo != null)
+        if (drawPVPIconsMethodInfo != null)
         {
             IL_Main.DrawPVPIcons -= Main_DrawPVPIcons_Hook;
         }
@@ -48,6 +63,17 @@ public class ClientModifications : ModSystem
         {
             // Log an error if we can't find the method, which might happen if the game updates.
             Mod.Logger.Error("Could not find Main.DrawPVPIcons method. PvP admin check will not work.");
+        }
+
+        var drawBubblesMethodInfo = typeof(Main).GetMethod("DrawPlayerChatBubbles", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (drawBubblesMethodInfo != null)
+        {
+            IL_Main.DrawPlayerChatBubbles -= DisableChatBubbles_Hook;
+        }
+        else
+        {
+            Mod.Logger.Error("Could not find Main.DrawPlayerChatBubbles method. Chat bubble disabling will not work.");
         }
     }
     private void TeamChangeFlag(ILContext iLContext)
@@ -80,7 +106,7 @@ public class ClientModifications : ModSystem
             i => i.MatchLdsfld(typeof(Main), nameof(Main.mouseLeftRelease)),
             i => i.MatchBrfalse(out _),
             i => i.MatchLdsfld(typeof(Main), nameof(Main.teamCooldown)),
-            i => i.MatchBrtrue(out endLabel) 
+            i => i.MatchBrtrue(out endLabel)
             ))
         {
             Mod.Logger.Error("Could not find injection point in Main.DrawPVPIcons. PvP admin check will not work.");
@@ -93,5 +119,41 @@ public class ClientModifications : ModSystem
         c.Emit(OpCodes.Callvirt, typeof(Player).GetMethod(nameof(Player.TeamChangeAllowed)));
         c.Emit(OpCodes.Brfalse, endLabel);
     }
+    private void DisableChatBubbles_Hook(ILContext il)
+    {
+        var c = new ILCursor(il);
+        c.Emit(OpCodes.Ret);
+    }
+        private void ChatHelper_DisplayMessage_Hook(ILContext il)
+    {
+        var c = new ILCursor(il);
 
+
+        if (!c.TryGotoNext(MoveType.Before, i => i.MatchLdfld<Player>("name")))
+        {
+            Mod.Logger.Error("Could not find injection point in ChatHelper.DisplayMessage. Team chat tags will not work.");
+            return;
+        }
+        
+        c.Emit(OpCodes.Ldarg_2); 
+        
+        c.EmitDelegate<System.Func<string, byte, string>>((originalName, messageAuthor) => {
+            Player player = Main.player[messageAuthor];
+
+            if (player.team > 0 && player.team < Main.teamColor.Length)
+            {
+                Mod.Logger.Info($"Hooked chat for '{originalName}' from author {messageAuthor}.");
+                Color teamColor = Main.teamColor[player.team];
+
+                string teamName = teamColor.ToString();
+                string teamTag = $"[c/{teamColor.Hex3()}:[{teamName}]]";
+
+                return $"{teamTag} {originalName}";
+            }
+            Mod.Logger.Info($"Failed");
+
+            return originalName;
+        });
+
+    }
 }
--- Content/Functionality/ChatPlayer.cs
+++ Content/Functionality/ChatPlayer.cs
@@ -1,4 +1,4 @@
-/* Not working rn
+
 using Terraria;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
@@ -23,38 +23,7 @@ namespace CTG2.Content
                 Main.chatText = "";
                 Main.NewText("You are muted and cannot chat.", Microsoft.Xna.Framework.Color.Red);
             }
-            else if (!string.IsNullOrEmpty(Main.chatText))
-            {
-                string teamTag = "";
-                string colorTag = "";
-                if (Player.team == 1)
-                {
-                    teamTag = "RED]";
-                    colorTag = "[c/FF4040:";
-                }
-                else if (Player.team == 3) // Blue
-                {
-                    teamTag = "BLUE]";
-                    colorTag = "[c/40A0FF:";
-                }
-                else
-                {
-                    teamTag = "NO TEAM]";
-                    colorTag = "[c/AAAAAA:";
-                }
-
-                if (Main.chatText.StartsWith("/"))
-                {
-                    
-                }
-
-                else if (!Main.chatText.StartsWith(colorTag + teamTag))
-                {
-
-                    Main.chatText = $"{colorTag}{teamTag}: {Main.chatText}";
-                }
-
-            }
+           
         }
     }
-} */
+} 
