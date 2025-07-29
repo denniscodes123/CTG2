207b3a0dde2c317c190ecc1fbfb5b40110179328
denniscodes123 <dadennis2005@gmail.com>
2025-07-29 14:26:41 -0400

    Create ClientConfig.cs

---
+++ Content/Configs/ClientConfig.cs
@@ -0,0 +1,16 @@
+using Terraria.ModLoader.Config;
+using System.ComponentModel;
+
+namespace CTG2.Content.Configs
+{
+    public class CTG2Config : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Label("Enable clash royale OT music")]
+        [DefaultValue(false)]
+        public bool ClashRoyaleOTMusic;
+
+
+    }
+}
