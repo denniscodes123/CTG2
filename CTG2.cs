2e40f71cc6b880827418cda595c29632d23f8489
genesisTerraria <abca41833@gmail.com>
2025-07-28 22:42:10 -0500

    Update CTG2.cs

--- CTG2.cs
+++ CTG2.cs
@@ -182,18 +182,18 @@ namespace CTG2
                     int projectileIndex = Projectile.NewProjectile(Main.LocalPlayer.GetSource_Misc("Class15Ability"), spawnPos, velocity, projType, damage, knockback);
                     break;
                 case (byte)MessageType.RequestAddBuff:
-                    var playerID = reader.ReadByte();
+                    var playerID = reader.ReadInt32();
                     var buffType = reader.ReadInt32();
                     var time = reader.ReadInt32();
 
-                    if (Main.player[playerID] is Player player)
-                    {
-                        Main.NewText("hitplayer", Color.Red);
-                        player.AddBuff(buffType, time);
-                    }
+                    Player player = Main.player[playerID];
+                    if (!player.active)
+                        break;
+                    
+                    player.AddBuff(buffType, time);
+                    NetMessage.SendData(MessageID.AddPlayerBuff, -1, -1, null, playerID, buffType, time);
 
                     break;
-
                 case (byte)MessageType.RequestNextMap:
                     string receivedMapName = reader.ReadString();
 
