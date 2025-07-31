d2ee74451dda2ab9bd949050d4d47a23779edf9c
genesisTerraria <abca41833@gmail.com>
2025-07-30 19:10:22 -0500

    prevents players from using abilities before class selection ends

--- Content/Classes/Abilities.cs
+++ Content/Classes/Abilities.cs
@@ -761,7 +761,7 @@ namespace CTG2.Content
                     break;
             }
 
-            if (((Player.HeldItem.type == ItemID.WhoopieCushion && Player.controlUseItem && Player.itemTime == 0) || CTG2.Ability1Keybind.JustPressed) && cooldown == 0) // Only activate if not on cooldown
+            if (((Player.HeldItem.type == ItemID.WhoopieCushion && Player.controlUseItem && Player.itemTime == 0) || CTG2.Ability1Keybind.JustPressed) && cooldown == 0 && playerManager.playerState == PlayerManager.PlayerState.Active) // Only activate if not on cooldown
             {
                 if (selectedClass == 1 || selectedClass == 4 || selectedClass == 7 || selectedClass == 11 || selectedClass == 13 || selectedClass == 17)
                     SoundEngine.PlaySound(abilityStart.WithVolumeScale(Main.soundVolume * 2f), Player.Center);
