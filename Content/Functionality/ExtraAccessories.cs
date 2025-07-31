bdf4b4d64d070ec746cd392f70acdb27fe0a2bcb
genesisTerraria <abca41833@gmail.com>
2025-07-30 23:40:29 -0500

    Update ExtraAccessories.cs

--- Content/Functionality/ExtraAccessories.cs
+++ Content/Functionality/ExtraAccessories.cs
@@ -1,13 +1,13 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
-using Terraria.Localization;
-using Terraria.ModLoader;
+// using Microsoft.Xna.Framework;
+// using Terraria;
+// using Terraria.ID;
+// using Terraria.Localization;
+// using Terraria.ModLoader;
 
 
-namespace CTG2.Content.Functionality
-{
-	public class AccessorySlot1 : ModAccessorySlot {}
+// namespace CTG2.Content.Functionality
+// {
+// 	public class AccessorySlot1 : ModAccessorySlot {}
 
-    public class AccessorySlot2 : ModAccessorySlot {}
-}
+//     public class AccessorySlot2 : ModAccessorySlot {}
+// }
