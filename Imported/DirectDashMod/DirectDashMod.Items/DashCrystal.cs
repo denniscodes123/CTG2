c2247a9971c5a53ff01a2c6ab319149b7ce934ec
denniscodes123 <dadennis2005@gmail.com>
2025-08-02 00:11:09 -0400

    Update DashCrystal.cs

--- Imported/DirectDashMod/DirectDashMod.Items/DashCrystal.cs
+++ Imported/DirectDashMod/DirectDashMod.Items/DashCrystal.cs
@@ -6,11 +6,11 @@ public class DashCrystal : SkippingStone
 {
 	public override string Texture => "CTG2/Imported/DirectDashMod/Items/DashCrystal";
 
-	public override float DashDist => 176f;
+	public override float DashDist => 146f;
 
 	public override float DashSpeed => 11.52f;
 
-	public override int DashCooldown => 15;
+	public override int DashCooldown => 60;
 
 	public override int DashCount => 1;
 
