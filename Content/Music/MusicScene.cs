2a7befa9405e9f4ecfe9f9662dfaefcd2f998b59
denniscodes123 <dadennis2005@gmail.com>
2025-08-05 00:56:48 -0400

    client side music

--- Content/Music/MusicScene.cs
+++ Content/Music/MusicScene.cs
@@ -1,37 +1,57 @@
 
-// using Microsoft.Xna.Framework;
-// using System;
-// using Terraria;
-// using Terraria.Graphics.Capture;
-// using Terraria.ID;
-// using Terraria.ModLoader;
-// using CTG2.Content.ServerSide;
-// using CTG2.Content.ClientSide;
-// using CTG2.Content.Configs;
+ using Microsoft.Xna.Framework;
+ using System;
+ using Terraria;
+ using Terraria.Graphics.Capture;
+ using Terraria.ID;
+ using Terraria.ModLoader;
+ using CTG2.Content.ServerSide;
+ using CTG2.Content.ClientSide;
+ using CTG2.Content.Configs;
 
 
-// namespace CTG2.Content
-// {
-//     // Shows setting up two basic biomes. For a more complicated example, please request.
-//     public class CTG2Biome : ModBiome
-//     {
-//         public override int Music
-//         {
-//             get
-//             {
-//                 var cfg = ModContent.GetInstance<CTG2Config>(); //reads from config
-//                 if (!cfg.ClashRoyaleOTMusic)
-//                     return -1; // this makes no ovveride
+namespace CTG2.Content
+{
+    /*
+      //Shows setting up two basic biomes. For a more complicated example, please request.
+    public class CTG2Biome : ModBiome
+    {
+        public override int Music
+        {
+            get
+            {
+                return MusicLoader.GetMusicSlot(Mod, "Assets/Music/clashroyaleOT");
+            }
+        }
 
-//                 return MusicLoader.GetMusicSlot(Mod, "Assets/Music/clashroyaleOT");
-//             }
-//         }
+        public override bool IsBiomeActive(Player player)
+            => ModContent.GetInstance<CTG2Config>().ClashRoyaleOTMusic; //make biome only active during overtime
+                                                                        //todo: Make sure isovertime is synced clientside
 
-//         public override bool IsBiomeActive(Player player)
-//             => GameInfo.overtime; //make biome only active during overtime
-//             //todo: Make sure isovertime is synced clientside
+        public override SceneEffectPriority Priority
+            => SceneEffectPriority.BiomeHigh;
+    } */
+    
 
-//         public override SceneEffectPriority Priority
-//             => SceneEffectPriority.BiomeHigh;
-//     }
-// }
+public class DynamicMusic : ModBiome
+{
+    public override bool IsBiomeActive(Player player) => true;
+    public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
+
+    public override int Music {
+        
+        get
+            {
+                int idx = ModContent.GetInstance<CTG2Config>().SelectedMusicIndex;
+                switch (idx)
+                {
+                    case 1: return MusicLoader.GetMusicSlot(Mod, "Assets/Music/clashroyaleOT");
+                    case 2: return MusicLoader.GetMusicSlot(Mod, "Assets/Music/MysteriousMystery");
+
+                    default:
+                        return -1;
+                }
+            }
+    }
+}
+}
