36d2d99f43b18c1b41d0b0e8d53292888931ce90
genesisTerraria <abca41833@gmail.com>
2025-12-29 21:37:38 -0600

    rate limited /die

--- Content/Commands/DieCommand.cs
+++ Content/Commands/DieCommand.cs
@@ -12,6 +12,8 @@ namespace CTG2.Content.Commands
         public override string Usage => "/die";
         public override string Description => "Kill yourself.";
 
+        private uint lastUsed = 0;
+
         public override void Action(CommandCaller caller, string input, string[] args)
         {
             if (args.Length > 0)
@@ -19,6 +21,14 @@ namespace CTG2.Content.Commands
                 caller.Reply("Usage: /die", Color.Red);
                 return;
             }
+
+            if (Main.GameUpdateCount - lastUsed < 120)
+            {
+                caller.Reply("Wait at least two seconds before using this command again!");
+                return;
+            }
+
+            lastUsed = Main.GameUpdateCount;
 
             // Send request to server to kill this player
             ModPacket packet = Mod.GetPacket();
